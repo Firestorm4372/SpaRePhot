a9b50ce9972f156239c43504064214e58dbb9343
Liam Brown <lffbrown@hotmail.co.uk>
2024-03-20 14:15:02 +0000

    Restructure to be more modular with plots

--- spare/viewer.py
+++ spare/viewer.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 from .galaxy import Galaxy, PhotGalaxy
 
-def show_galaxy(galaxy:Galaxy, normalise_separate:bool=True, config_file:str='config.yml') -> plt.Figure:
 
-    # segmap
+def _ax_segmap(ax:plt.Axes, galaxy:Galaxy) -> None:
     galaxy_truth = np.equal(galaxy.segmap, galaxy.id)
     any_object_truth = np.not_equal(galaxy.segmap, 0)
     segmap_image = 0.5 * galaxy_truth + 0.5 * any_object_truth
 
+    ax.imshow(segmap_image, origin='lower')
+
+def _ax_rgb(ax:plt.Axes, galaxy:Galaxy, normalise_separate:bool, config_file:str) -> None:
     # colors
     with open(config_file) as f:
         config = yaml.safe_load(f)
@@ -35,19 +37,10 @@ def show_galaxy(galaxy:Galaxy, normalise_separate:bool=True, config_file:str='co
 
     rgb = np.moveaxis([channels[color] for color in colors], 0, -1)
 
-    fig, axs = plt.subplots(1, 2, sharex=True, sharey=True)
-    axs:list[plt.Axes]
-    axs[0].imshow(segmap_image, origin='lower')
-    axs[1].imshow(rgb, origin='lower')
+    ax.imshow(rgb, origin='lower')
 
-    return fig
-
-
-def show_redshift(galaxy:PhotGalaxy, show_text:bool=True) -> plt.Figure:
+def _ax_redshift(ax:plt.Axes, galaxy:PhotGalaxy, show_text:bool) -> None:
     zbest = galaxy.zbest_reshaped()
-
-    fig, ax = plt.subplots()
-    ax:plt.Axes
     ax.imshow(zbest, cmap='Reds', origin='lower')
 
     if show_text:
@@ -55,5 +48,30 @@ def show_redshift(galaxy:PhotGalaxy, show_text:bool=True) -> plt.Figure:
             for j, z in enumerate(row):
                 _ = ax.text(j, i, f'{z:.1f}', ha='center', va='center', color='w')
 
+
+def segmap_image(galaxy:Galaxy, normalise_separate:bool=True, config_file:str='config.yml') -> plt.Figure:
+    fig, axs = plt.subplots(1, 2, sharex=True, sharey=True)
+    _ax_segmap(axs[0], galaxy)
+    _ax_rgb(axs[1], galaxy, normalise_separate, config_file)
+
+    fig.tight_layout()
+    return fig
+
+
+def redshift(galaxy:PhotGalaxy, show_text:bool=False) -> plt.Figure:
+    fig, ax = plt.subplots()
+    _ax_redshift(ax, galaxy, show_text)
+
+    fig.tight_layout()
+    return fig
+
+
+def segmap_image_redshift(galaxy:PhotGalaxy, normalise_separate:bool=True, show_text:bool=False, config_file:str='config.yml') -> plt.Figure:
+    fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
+    _ax_segmap(axs[0], galaxy)
+    _ax_rgb(axs[1], galaxy, normalise_separate, config_file)
+    _ax_redshift(axs[2], galaxy, show_text)
+
+    fig.tight_layout()
     return fig
 
